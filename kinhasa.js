4db5e7dc87932c450f6e7d692498574449f37b4f
Yash Aggarwal <yashaggarwal3011@gmail.com>
2024-05-25 20:54:42 +0530

    hmmm

--- kinhasa.js
+++ kinhasa.js
@@ -1,8 +1,8 @@
 // Initialize the map
 var map = new maptalks.Map('map', {
-    center: [15.2663, -4.4419],
+    center: [15.2663, -4.4419], // Coordinates for Kinshasa
     zoom: 14.1,
-    minZoom: 14,
+    minZoom: 14, // Minimum zoom level
     centerCross: true,
     baseLayer: new maptalks.TileLayer('base', {
         urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
@@ -29,13 +29,30 @@ map.getLayer('v').addGeometry(
 // Allow zooming in but not zooming out
 map.setMinZoom(14);
 
-// Sample heat map data points
+// Simulated heat map data points for bad AQI
 var data = [
-    [15.2663, -4.4419],
-    [15.2683, -4.4429],
-    [15.2693, -4.4439],
-    [15.2703, -4.4449],
-    [15.2713, -4.4459]
+    [15.2663, -4.4419, 10],
+    [15.2683, -4.4429, 9],
+    [15.2753, -4.4499, 4],
+    [15.2763, -4.4509, 8],
+    [15.2773, -4.4519, 9],
+    [15.2783, -4.4529, 10],
+    [15.2833, -4.4579, 9],
+    [15.2843, -4.4589, 6],
+    [15.2853, -4.4599, 7],
+    [15.2863, -4.4609, 5],
+    [15.2600, -4.4400, 8],
+    [15.2650, -4.4410, 7],
+    [15.2640, -4.4420, 9],
+    [15.2610, -4.4450, 8],
+    [15.2600, -4.4460, 7],
+    [15.2590, -4.4470, 9],
+    [15.2580, -4.4480, 6],
+    [15.2570, -4.4490, 5],
+    [15.2530, -4.4530, 6],
+    [15.2520, -4.4540, 5],
+    [15.2510, -4.4550, 8],
+    [15.2500, -4.4560, 7]
 ];
 
 // Create the heat map layer and add it to the map
@@ -44,8 +61,24 @@ var heatlayer = new maptalks.HeatLayer('heat', data, {
     'forceRenderOnRotating': true,
     'forceRenderOnMoving': true,
     'forceRenderOnZooming': true,
-    'radius': 15
+    'radius': 25,
+    'max': 10,
+    'blur': 20,
+    'gradient': {
+        0.0: 'blue',
+        0.5: 'lime',
+        1.0: 'red'
+    }
 }).addTo(map);
 
 // Log the heat layer to the console to ensure it was added
 console.log("Heat layer added:", heatlayer);
+
+// Add a recenter button
+document.querySelector('.button-container').innerHTML += '<button class="icon-button" id="recenter-button"><i class="fas fa-crosshairs"></i></button>';
+
+// Recenter map function
+document.getElementById('recenter-button').addEventListener('click', function() {
+    map.setCenter([15.2663, -4.4419]);
+    map.setZoom(14.2);
+});
